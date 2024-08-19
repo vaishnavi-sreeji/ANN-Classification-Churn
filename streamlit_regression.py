5e23751e778a548aeaa58ac15af751e75d623e1b
Vaishnavi S.J <145783406+vaishnavi-sreeji@users.noreply.github.com>
2024-08-19 14:23:00 +0530

    Add files via upload

---
+++ streamlit_regression.py
@@ -0,0 +1,61 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+from sklearn.preprocessing import StandardScaler,LabelEncoder,OneHotEncoder
+import pickle
+from tensorflow.keras.models import load_model
+
+model=load_model("regression_model.h5")
+
+with open("le.pkl","rb") as file:
+    le=pickle.load(file)
+with open("ohe.pkl" ,"rb") as file:
+    ohe=pickle.load(file)
+with open("scaler.pkl" ,"rb") as file:
+    scaler=pickle.load(file)
+
+st.title("Estimated Salary Prediction")
+
+geography=st.selectbox("Geography",ohe.categories_[0])
+gender=st.selectbox("Gender",le.classes_)
+age=st.slider("Age",0,100,25)
+balance=st.number_input("Balance")
+credit_score=st.number_input("CreditScore")
+exited=st.selectbox("Exited",[0,1])
+tenure=st.slider("tenure",0,10)
+num_of_products=st.slider("Number of Products",11,4)
+has_cr_card=st.selectbox("has Credit Card",[0,1])
+is_active_number=st.selectbox("Is Active Member",[0,1])
+
+
+input_data=pd.DataFrame({
+    "CreditScore":[credit_score],
+    "Gender":[le.transform([gender])[0]],
+    "Age":[age],
+    "Tenure":[tenure],
+    "Balance":[balance],
+    "NumOfProducts":[num_of_products],
+    "HasCrCard":[has_cr_card],
+    "IsActiveMember":[is_active_number],
+    "Exited":[exited]
+
+
+})
+
+geo_encoded=ohe.transform([[geography]]).toarray()
+ge_encoded_df=pd.DataFrame(geo_encoded,columns=ohe.get_feature_names_out(["Geography"]))
+# input_data=pd.concat([input_data.drop('Geography',axis=1),ge_encoded_df],axis=1)
+input_data = pd.concat([input_data.reset_index(drop=True), ge_encoded_df], axis=1)
+
+
+
+# expected_columns = scaler.feature_names_in_
+# input_data = input_data.reindex(columns=expected_columns)
+
+input_scaled=scaler.transform(input_data)
+
+prediction=model.predict(input_scaled)
+
+prediction_proba=prediction[0][0]
+st.write("Predicted Estimated Salary:{}".format(prediction_proba))
